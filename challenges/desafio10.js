9c787fee774d2ade0516bdbf325fafcb3d69c0e7
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-12-02 08:40:29 -0300

    Desafio10.

---
+++ challenges/desafio10.js
@@ -0,0 +1,34 @@
+db.trips.aggregate(
+  [
+    {
+      $project: {
+        _id: 0,
+        usertype: "$usertype",
+        time: {
+          $divide: [
+            {
+              $dateDiff: {
+                startDate: "$startTime",
+                endDate: "$stopTime",
+                unit: "millisecond",
+              },
+            }, 3.6e6,
+          ],
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$usertype",
+        time: { $avg: "$time" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        tipo: "$_id",
+        duracaoMedia: { $round: ["$time", 2] },
+      },
+    },
+  ],
+);
